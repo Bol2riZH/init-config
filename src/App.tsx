a708d40e0c69601621d1273d01117845908b20c9
Matthieu <reungoat.matthieu@gmail.com>
2024-01-07 22:45:53 +0100

    add all ui components from shadcn - change theme to green

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,191 @@
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { Card, CardContent } from '@/components/ui/card';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel';
+import { Minus, Plus } from 'lucide-react';
+import { Bar, BarChart, ResponsiveContainer } from 'recharts';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
+import { Label } from '@/components/ui/label';
+import { Switch } from '@/components/ui/switch';
+
+const data = [
+  {
+    goal: 400,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 278,
+  },
+  {
+    goal: 189,
+  },
+  {
+    goal: 239,
+  },
+  {
+    goal: 300,
+  },
+  {
+    goal: 200,
+  },
+  {
+    goal: 278,
+  },
+  {
+    goal: 189,
+  },
+  {
+    goal: 349,
+  },
+];
+
 const App = () => {
+  const [goal, setGoal] = useState(350);
+
+  function onClick(adjustment: number) {
+    setGoal(Math.max(200, Math.min(400, goal + adjustment)));
+  }
   return (
-    <>
-      <h1>HELLO WORLD</h1>
-    </>
+    <div className='grid place-items-center h-screen'>
+      <div className='flex items-center space-x-2'>
+        <Switch id='airplane-mode' />
+        <Label htmlFor='airplane-mode'>Airplane Mode</Label>
+      </div>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button variant='outline'>Hover</Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Add to library</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant='outline'>Show Dialog</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <Carousel className='w-full max-w-xs'>
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index}>
+              <div className='p-1'>
+                <Card>
+                  <CardContent className='flex aspect-square items-center justify-center p-6'>
+                    <span className='text-4xl font-semibold'>{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button variant='outline'>Open Drawer</Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <div className='mx-auto w-full max-w-sm'>
+            <DrawerHeader>
+              <DrawerTitle>Move Goal</DrawerTitle>
+              <DrawerDescription>Set your daily activity goal.</DrawerDescription>
+            </DrawerHeader>
+            <div className='p-4 pb-0'>
+              <div className='flex items-center justify-center space-x-2'>
+                <Button variant='outline' size='icon' className='h-8 w-8 shrink-0 rounded-full' onClick={() => onClick(-10)} disabled={goal <= 200}>
+                  <Minus className='h-4 w-4' />
+                  <span className='sr-only'>Decrease</span>
+                </Button>
+                <div className='flex-1 text-center'>
+                  <div className='text-7xl font-bold tracking-tighter'>{goal}</div>
+                  <div className='text-[0.70rem] uppercase text-muted-foreground'>Calories/day</div>
+                </div>
+                <Button variant='outline' size='icon' className='h-8 w-8 shrink-0 rounded-full' onClick={() => onClick(10)} disabled={goal >= 400}>
+                  <Plus className='h-4 w-4' />
+                  <span className='sr-only'>Increase</span>
+                </Button>
+              </div>
+              <div className='mt-3 h-[120px]'>
+                <ResponsiveContainer width='100%' height='100%'>
+                  <BarChart data={data}>
+                    <Bar
+                      dataKey='goal'
+                      style={
+                        {
+                          fill: 'hsl(var(--foreground))',
+                          opacity: 0.9,
+                        } as React.CSSProperties
+                      }
+                    />
+                  </BarChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
+            <DrawerFooter>
+              <Button>Submit</Button>
+              <DrawerClose asChild>
+                <Button variant='outline'>Cancel</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </div>
+        </DrawerContent>
+      </Drawer>
+      <div className='flex items-center space-x-4'>
+        <Skeleton className='h-12 w-12 rounded-full' />
+        <div className='space-y-2'>
+          <Skeleton className='h-4 w-[250px]' />
+          <Skeleton className='h-4 w-[200px]' />
+        </div>
+      </div>
+    </div>
   );
 };
 
